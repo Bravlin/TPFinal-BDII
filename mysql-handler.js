8e69bc4b60c896a1193d2cdf4701527a5278bf86
Bravlin <bravlinleonel@gmail.com>
2019-12-04 15:30:25 -0300

    Migracion implementada

---
+++ mysql-handler.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql');
+const { execSync } = require('child_process');
+
+module.exports.pingDatabase = (host, user, password) => {
+    let dbcon = mysql.createConnection({
+        host: host,
+        user: user,
+        password: password,
+    });
+    
+    dbcon.ping(err => {
+        if (err) throw err;
+        console.log("Connected to MySQL.");
+        dbcon.end();
+    });
+}
+
+module.exports.createDatabase = (host, user, password, database_name) => {
+    let dbcon = mysql.createConnection({
+        host: host,
+        user: user,
+        password: password,
+    });
+
+    dbcon.query("CREATE DATABASE " + database_name, (err, result) => {
+        if (err) throw err;
+        console.log("Database created.");
+        dbcon.end();
+    });
+}
+
+module.exports.migrateDatabase = (host, user, password, database) => {
+    try {
+        execSync("mysql -h " + host + " -u " + user + " --password=" + password + " " + database + " < migration.sql", {stdio: 'ignore'});
+        console.log("Database migration successful.");
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
